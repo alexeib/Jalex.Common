4083a75a766bf1db27977e9bc5c3e803faee51a0
Alex <a@b.com>
2014-04-25 19:39:23 -0400

    moved ClusteringKey attribute to infrastructure

---
+++ Jalex.Infrastructure/Attributes/ClusteringKeyAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Jalex.Infrastructure.Attributes
+{
+    /// <summary>
+    /// Note that this attribute is used ONLY by Cassandra repository
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, Inherited = true, AllowMultiple = true)]
+    public class ClusteringKeyAttribute : Attribute
+    {
+        public ClusteringKeyAttribute(int index) { Index = index; }
+        /// <summary>
+        /// Sets the clustering key and optionally a clustering order for it.
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="order">Use "DESC" for descending order and "ASC" for ascending order.</param>
+        public ClusteringKeyAttribute(int index, string order)
+        {
+            Index = index;
+
+            if (order == "DESC" || order == "ASC")
+                ClusteringOrder = order;
+            else
+                throw new ArgumentException("Possible arguments are: \"DESC\" - for descending order and \"ASC\" - for ascending order.");
+        }
+        public int Index = -1;
+        public string ClusteringOrder = null;
+        public string Name;
+    }
+}
--- Jalex.Repository/Cassandra/DataStax.Linq/Table.cs
+++ Jalex.Repository/Cassandra/DataStax.Linq/Table.cs
@@ -44,30 +44,7 @@ namespace Jalex.Repository.Cassandra.DataStax.Linq
         public ColumnAttribute() {}
         public ColumnAttribute(string Name) { this.Name = Name; }
         public string Name = null;
-    }
-
-    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, Inherited = true, AllowMultiple = true)]
-    public class ClusteringKeyAttribute : Attribute
-    {
-        public ClusteringKeyAttribute(int index) { this.Index = index; }
-        /// <summary>
-        /// Sets the clustering key and optionally a clustering order for it.
-        /// </summary>
-        /// <param name="index"></param>
-        /// <param name="order">Use "DESC" for descending order and "ASC" for ascending order.</param>
-        public ClusteringKeyAttribute(int index, string order) 
-        { 
-            this.Index = index;
-            
-            if (order == "DESC" || order == "ASC")
-                this.ClusteringOrder = order;
-            else
-                throw new ArgumentException("Possible arguments are: \"DESC\" - for descending order and \"ASC\" - for ascending order.");
-        }
-        public int Index = -1;
-        public string ClusteringOrder = null;
-        public string Name;
-    }
+    }    
 
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, Inherited = true, AllowMultiple = true)]
     public class CounterAttribute : Attribute
