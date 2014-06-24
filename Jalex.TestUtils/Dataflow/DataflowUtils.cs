3d08b9b10ad45bed6e309fdab6c2366b81d37281
Alex <a@b.com>
2014-06-23 23:20:43 -0400

    added dataflow helper methods to test utils

---
+++ Jalex.TestUtils/Dataflow/DataflowUtils.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks.Dataflow;
+
+namespace Jalex.TestUtils.Dataflow
+{
+    public static class DataflowUtils
+    {
+        public static IEnumerable<T> GetDataFromBlock<T>(ISourceBlock<T> source)
+        {
+            ConcurrentBag<T> results = new ConcurrentBag<T>();
+            var pusherBlock = new ActionBlock<T>(d => results.Add(d), new ExecutionDataflowBlockOptions { SingleProducerConstrained = true });
+            source.LinkTo(pusherBlock, new DataflowLinkOptions { PropagateCompletion = true });
+            pusherBlock.Completion.Wait();
+            return results;
+        }
+    }
+}
