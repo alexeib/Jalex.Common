594ceb257bb2160374b7d8f436c642c23176d8f2
Alex <alexei.b@gmail.com>
2014-08-18 19:08:58 -0400

    updated packages, added data flow extensions to infrastructure project

--- Jalex.Infrastructure/Extensions/DataflowExtensions.cs
+++ Jalex.Infrastructure/Extensions/DataflowExtensions.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using System.Threading.Tasks.Dataflow;
 
-namespace Jalex.TestUtils.Dataflow
+namespace Jalex.Infrastructure.Extensions
 {
-    public static class DataflowUtils
+    public static class DataflowExtensions
     {
-        public static IEnumerable<T> GetDataFromBlock<T>(ISourceBlock<T> source)
+        public static IEnumerable<T> GetDataFromBlock<T>(this ISourceBlock<T> source)
         {
             ConcurrentBag<T> results = new ConcurrentBag<T>();
             var pusherBlock = new ActionBlock<T>(d => results.Add(d), new ExecutionDataflowBlockOptions { SingleProducerConstrained = true });
