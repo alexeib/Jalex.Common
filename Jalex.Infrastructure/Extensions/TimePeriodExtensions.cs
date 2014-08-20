e3c6314bd3f45ea4c7bff7e5eb1dcb4d4bdaf8a0
Alex <alexei.b@gmail.com>
2014-08-20 07:09:43 -0400

    Added time period extensions to infrastructure (moved from test)

---
+++ Jalex.Infrastructure/Extensions/TimePeriodExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Itenso.TimePeriod;
+
+namespace Jalex.Infrastructure.Extensions
+{
+    public static class TimePeriodExtensions
+    {
+        public static void ForEachDay(
+            this IEnumerable<ITimePeriod> timePeriods,
+            Action<DateTime> action)
+        {
+            foreach (var timePeriod in timePeriods)
+            {
+                if (!timePeriod.HasStart)
+                {
+                    throw new InvalidOperationException("time period must have a start");
+                }
+
+                if (!timePeriod.HasEnd)
+                {
+                    throw new InvalidOperationException("time period must have an end");
+                }
+
+                DateTime currDate = timePeriod.Start;
+                while (currDate <= timePeriod.End)
+                {
+                    action(currDate);
+                    currDate = currDate.AddDays(1);
+                }
+            }
+        }
+    }
+}
