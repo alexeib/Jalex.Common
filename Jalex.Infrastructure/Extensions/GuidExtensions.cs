91de08ef953075bb4e6b916affeab98ac05c450b
Alex <a@b.com>
2015-04-07 00:32:52 -0400

    an extension method for guids

---
+++ Jalex.Infrastructure/Extensions/GuidExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jalex.Infrastructure.Extensions
+{
+    public static class GuidExtensions
+    {
+        public static bool IsNotEmpty(this Guid guid)
+        {
+            return guid != Guid.Empty;
+        }
+    }
+}
