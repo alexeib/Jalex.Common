7f1eeda266aecf4979711b5b2bd3517440fae4d5
Alex <alexei.b@gmail.com>
2015-11-28 19:03:10 +0000

    added thread-safe random

---
+++ Jalex.Infrastructure/Utils/ThreadsafeRandom.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Jalex.Infrastructure.Utils
+{
+
+
+    public static class ThreadsafeRandom
+    {
+        private static readonly RNGCryptoServiceProvider _global = new RNGCryptoServiceProvider();
+
+        [ThreadStatic]
+        private static Random _local;
+
+        public static int Next()
+        {
+            Random inst = _local;
+            if (inst == null)
+            {
+                byte[] buffer = new byte[4];
+                _global.GetBytes(buffer);
+                _local = inst = new Random(
+                    BitConverter.ToInt32(buffer, 0));
+            }
+            return inst.Next();
+        }
+    }
+}
