49fad0d1614f9e7629cbab145814469beb575b52
Alex <alexei.b@gmail.com>
2015-12-06 16:34:52 +0000

    added timed log

---
+++ Jalex.Services/Logging/TimedLog.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using Magnum;
+using NLog;
+
+namespace Jalex.Services.Logging
+{
+    public class TimedLog : IDisposable
+    {
+        private readonly string _message;
+        private readonly ILogger _logger;
+        private readonly Stopwatch _timer;
+
+        public static TimedLog Log(ILogger logger, string message)
+        {
+            return new TimedLog(logger, message);
+        }
+
+        public TimedLog(ILogger logger, string message)
+        {
+            Guard.AgainstNull(logger, "logger");
+
+            _logger = logger;
+            _message = message;
+
+            _logger.Info("Starting: {0}", _message);
+
+            _timer = new Stopwatch();
+            _timer.Start();
+        }
+
+        [DebuggerNonUserCode]
+        public void Dispose()
+        {
+            _timer.Stop();
+            _logger.Info("{0}: {1}ms", _message, _timer.Elapsed.TotalMilliseconds);
+        }
+    }
+}
