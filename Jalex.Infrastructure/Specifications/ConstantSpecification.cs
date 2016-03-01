ad0d22a3dfd1a7aff3358560f8ef9a4d533f87b1
Alex <alexei.b@gmail.com>
2016-03-01 15:47:17 +0000

    added constant specification

---
+++ Jalex.Infrastructure/Specifications/ConstantSpecification.cs
@@ -0,0 +1,21 @@
+﻿namespace Jalex.Infrastructure.Specifications
+{
+    public class ConstantSpecification<T> : ISpecification<T>
+    {
+        private readonly bool _constantValue;
+
+        public ConstantSpecification(bool constantValue)
+        {
+            _constantValue = constantValue;
+        }
+
+        #region Implementation of ISpecification<in T>
+
+        public bool IsSatisfiedBy(T instance)
+        {
+            return _constantValue;
+        }
+
+        #endregion
+    }
+}
