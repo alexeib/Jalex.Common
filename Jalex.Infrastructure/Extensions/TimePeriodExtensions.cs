606abecec0b173d05a3a91f2edcf7bb249f7629a
Alex <alexei.b@gmail.com>
2016-03-02 21:46:06 +0000

    additional time period extension

--- Jalex.Infrastructure/Extensions/TimePeriodExtensions.cs
+++ Jalex.Infrastructure/Extensions/TimePeriodExtensions.cs
@@ -6,9 +6,15 @@ namespace Jalex.Infrastructure.Extensions
 {
     public static class TimePeriodExtensions
     {
-        public static void ForEachDay(
-            this IEnumerable<ITimePeriod> timePeriods,
-            Action<DateTime> action)
+        public static void ForEachDay(this IEnumerable<ITimePeriod> timePeriods, Action<DateTime> action)
+        {
+            foreach (var date in timePeriods.Dates())
+            {
+                action(date);
+            }
+        }
+
+        public static IEnumerable<DateTime> Dates(this IEnumerable<ITimePeriod> timePeriods)
         {
             foreach (var timePeriod in timePeriods)
             {
@@ -25,7 +31,7 @@ namespace Jalex.Infrastructure.Extensions
                 DateTime currDate = timePeriod.Start;
                 while (currDate <= timePeriod.End)
                 {
-                    action(currDate);
+                    yield return currDate;
                     currDate = currDate.AddDays(1);
                 }
             }
