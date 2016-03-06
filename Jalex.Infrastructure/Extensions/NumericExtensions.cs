39316a6c09aae4027e9e51cb8ebc2795424b335e
Alex <alexei.b@gmail.com>
2016-03-06 16:38:38 +0000

    added numeric extensions

---
+++ Jalex.Infrastructure/Extensions/NumericExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Jalex.Infrastructure.Extensions
+{
+    public static class NumericExtensions
+    {
+        public static bool IsNaNOrInfinity(this double val)
+        {
+            return double.IsNaN(val) || double.IsInfinity(val);
+        }
+    }
+}
