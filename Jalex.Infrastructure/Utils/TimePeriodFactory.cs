793f3dc2f36f256584cf11125fc3b3eff94493f4
Alex <alexei.b@gmail.com>
2016-03-08 17:36:52 +0000

    added convenience methods for itenso lib

---
+++ Jalex.Infrastructure/Utils/TimePeriodFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Itenso.TimePeriod;
+using Jalex.Infrastructure.Extensions;
+
+namespace Jalex.Infrastructure.Utils
+{
+    public static class TimePeriodFactory
+    {
+        public static ITimePeriodCollection FromStartEnd(DateTime startDate, DateTime endDate)
+        {
+            return new TimePeriodCollection(new TimeBlock(startDate, endDate).ToEnumerable());
+        }
+
+        public static ITimePeriodCollection FromStartEnd(DateTime startDate, DateTime endDate, DateTimeKind kind)
+        {
+            return FromStartEnd(DateTime.SpecifyKind(startDate, kind), DateTime.SpecifyKind(endDate, kind));
+        }
+    }
+}
