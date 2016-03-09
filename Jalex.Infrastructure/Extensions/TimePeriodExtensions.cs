0e8348d2842f5d2dbba6965fa0390d167b6ad931
Alex <alexei.b@gmail.com>
2016-03-09 09:46:32 +0000

    added distinct dates flag

--- Jalex.Infrastructure/Extensions/TimePeriodExtensions.cs
+++ Jalex.Infrastructure/Extensions/TimePeriodExtensions.cs
@@ -1,21 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Itenso.TimePeriod;
 
 namespace Jalex.Infrastructure.Extensions
 {
     public static class TimePeriodExtensions
     {
-        public static void ForEachDay(this IEnumerable<ITimePeriod> timePeriods, Action<DateTime> action)
+        public static void ForEachDay(this IEnumerable<ITimePeriod> timePeriods, bool distinct, Action<DateTime> action)
         {
-            foreach (var date in timePeriods.Dates())
+            foreach (var date in timePeriods.Dates(distinct))
             {
                 action(date);
             }
         }
 
-        public static IEnumerable<DateTime> Dates(this IEnumerable<ITimePeriod> timePeriods)
+        public static ITimePeriodCollection Merge(this IEnumerable<ITimePeriod> timePeriods)
+        {            
+            var merged = new TimePeriodCollection();
+
+            var sorted = timePeriods.OrderBy(p => p.Start)
+                                    .ToList();
+
+            if (sorted.Count == 0)
+                return merged;
+
+            DateTime currStart = sorted[0].Start;
+            DateTime currEnd = sorted[0].End;
+            
+
+            foreach (var next in sorted.Skip(1))
+            {
+                if (next.Start > currEnd)
+                {
+                    merged.Add(new TimeBlock(currStart, currEnd));
+                    currStart = next.Start;
+                    currEnd = next.End;
+                }
+                else if (next.End > currEnd)
+                {
+                    currEnd = next.End;
+                }
+            }
+
+            merged.Add(new TimeBlock(currStart, currEnd));
+
+            return merged;
+        }
+
+        public static IEnumerable<DateTime> Dates(this IEnumerable<ITimePeriod> timePeriods, bool distinct)
         {
+            if (distinct)
+            {
+                timePeriods = timePeriods.Merge();
+            }
+
             foreach (var timePeriod in timePeriods)
             {
                 if (!timePeriod.HasStart)
